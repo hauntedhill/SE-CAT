f5ad04d700ce4a60f0f2b90c523d554392c076e8
zuch1000 <zuch1000@stud.hs-coburg.de>
2014-05-06 19:16:53 +0200

    added dynamic fxml loading

--- src/main/java/de/hscoburg/evelin/secat/controller/LayoutController.java
+++ src/main/java/de/hscoburg/evelin/secat/controller/LayoutController.java
@@ -5,19 +5,33 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
 
 import org.springframework.stereotype.Controller;
 
+import de.hscoburg.evelin.secat.util.spring.SpringFXMLLoader;
+
 @Controller
 public class LayoutController implements Initializable {
+
+	private static final String GUI_PATH = "/gui/";
+
+	private static final String STAMMDATEN_PATH = GUI_PATH + "stammdaten/";
+
+	public static final String HANDLUNGSFELD_FXML = STAMMDATEN_PATH + "handlungsfeld.fxml";
+
+	public static final String TOPMENU_FXML = GUI_PATH + "TopMenu.fxml";
 
 	@FXML
 	private BorderPane layout;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		System.out.println("####" + layout);
+
+		layout.setTop((Node) SpringFXMLLoader.getInstance().load(TOPMENU_FXML));
+
+		layout.setCenter((Node) SpringFXMLLoader.getInstance().load(HANDLUNGSFELD_FXML));
 
 	}
 
