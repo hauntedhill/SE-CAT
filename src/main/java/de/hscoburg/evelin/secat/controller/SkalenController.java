4266d07e0d22a749f8a7af1c04ca74f1afee68d5
zuch1000 <zuch1000@stud.hs-coburg.de>
2014-05-08 22:24:18 +0200

    fix focus and key events for buttons

--- src/main/java/de/hscoburg/evelin/secat/controller/SkalenController.java
+++ src/main/java/de/hscoburg/evelin/secat/controller/SkalenController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
@@ -13,6 +14,8 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.util.Callback;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +45,7 @@ public class SkalenController extends BaseController {
 	public void initializeController(URL location, ResourceBundle resources) {
 
 		loadList();
+		textNameSkalen.requestFocus();
 		listSkalen.setCellFactory(new Callback<ListView<Skala>, ListCell<Skala>>() {
 
 			@Override
@@ -72,6 +76,26 @@ public class SkalenController extends BaseController {
 				Skala e = new Skala();
 				e.setName(textNameSkalen.getText());
 				skalenModel.persist(e);
+
+			}
+
+			@Override
+			public void updateUI() {
+				loadList();
+			}
+		});
+
+		buttonAdd.setOnKeyPressed(new SeCatEventHandle<Event>() {
+
+			@Override
+			public void handleAction(Event event) {
+				if (((KeyEvent) event).getCode() == KeyCode.ENTER)
+
+				{
+					Skala e = new Skala();
+					e.setName(textNameSkalen.getText());
+					skalenModel.persist(e);
+				}
 
 			}
 
